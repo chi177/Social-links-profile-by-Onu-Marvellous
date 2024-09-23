b634ccb9227012bb5f05163ff720119ae6689e9c
chi177 <marvellousonu147@gmail.com>
2024-09-23 11:45:57 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Select the user information elements
+const userName = document.querySelector('Jessica Randal');
+const userLocation = document.querySelector('.User-location');
+const userDescription = document.querySelector('.user-description');
+
+// Update the user information dynamically
+userName.textContent = 'New User Name';
+userLocation.textContent = 'New Location';
+userDescription.textContent = 'New Description';
